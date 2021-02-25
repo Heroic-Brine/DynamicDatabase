b6b21c4beb20a9a44d740d55131600df1922c294
Anton John Erik Sundström <sundstrom85@gmail.com>
2021-02-25 12:40:35 +0100

    Started adding code for logging in

---
+++ Data/HashingSHA256.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
+using DynamicDatabase.Interfaces;
+
+namespace DynamicDatabase.Data
+{
+    public class HashingSHA256 : IHashing
+    {        
+        public string GenerateHash(string input)
+        {
+            string finalHashString;
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                finalHashString = BytesToString(bytes);
+            }
+            return finalHashString;
+        }
+        private string BytesToString(byte[] bytes)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}
